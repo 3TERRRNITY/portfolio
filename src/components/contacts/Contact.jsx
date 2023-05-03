cedb631ad973018519d621af66bdebe7b914f57c
3TERRRNITY <qertisww@gmail.com>
2023-05-03 21:27:07 +0300

    Добавил блок с контактами

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Contact.css";
+import { AiOutlineArrowRight, AiOutlineMail } from "react-icons/ai";
+import { TbBrandTelegram } from "react-icons/tb";
 
 const Contact = () => {
   return (
@@ -10,9 +12,74 @@ const Contact = () => {
       <div className="contact__container container grid">
         <div className="contact__content">
           <h3 className="contact__title">Talk to me</h3>
+
+          <div className="contact__info">
+            <div className="contact__card">
+              <AiOutlineMail className="contact__card-icon"></AiOutlineMail>
+
+              <h3 className="contact__card-title">Email</h3>
+              <span className="contact__card-data">
+                fedor.nedbalskij@gmail.com
+              </span>
+
+              <a
+                href="mailto:fedor.nedbalskij@gmail.com"
+                className="contact__button"
+              >
+                Write me{" "}
+                <AiOutlineArrowRight className="contact__button-icon"></AiOutlineArrowRight>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <TbBrandTelegram className="contact__card-icon"></TbBrandTelegram>
+
+              <h3 className="contact__card-title">Telegram</h3>
+              <span className="contact__card-data">@threename</span>
+
+              <a href="https://t.me/threename" className="contact__button">
+                Write me{" "}
+                <AiOutlineArrowRight className="contact__button-icon"></AiOutlineArrowRight>
+              </a>
+            </div>
+          </div>
         </div>
         <div className="contact__content">
           <h3 className="contact__title">Write me your project</h3>
+
+          <form className="contact__form">
+            <div className="contact__form-div">
+              <label className="contact__form-tag">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="contact__form-input"
+                placeholder="Insert your name"
+              />
+            </div>
+
+            <div className="contact__form-div">
+              <label className="contact__form-tag">Mail</label>
+              <input
+                type="email"
+                name="email"
+                className="contact__form-input"
+                placeholder="Insert your name"
+              />
+            </div>
+
+            <div className="contact__form-div contact__form-area">
+              <label className="contact__form-tag">Project</label>
+              <textarea
+                name="project"
+                cols={"30"}
+                rows={"10"}
+                className="contact__form-input"
+                placeholder="Describe your project"
+              />
+            </div>
+            <button className="button button--flex">Send Message</button>
+          </form>
         </div>
       </div>
     </section>
